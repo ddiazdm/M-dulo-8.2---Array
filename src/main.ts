642fb0f378417074a54b33b69cf28a7d20fc6947
danielddm <daniel.diazdelgado@invesyde.com>
2024-09-19 07:26:26 +0200

    first

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+
+console.log("Hello Typescript!");
+
+const obtenPacientesAsignadosAPediatria = (
+    pacientes: Pacientes[]
+  ): Pacientes[] => {
+    // Tu implementación aquí :)
+  };
+  
